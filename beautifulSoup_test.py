9a1521d9c1dc29ec48717fb8192ca91b559166ef
AustinDamn <hct830@gmail.com>
2016-03-17 14:58:13 +0800

    first stage

---
+++ beautifulSoup_test.py
@@ -0,0 +1,14 @@
+import urllib
+from BeautifulSoup import *
+
+url = raw_input('Enter = ')
+html = urllib.urlopen(url).read()
+soup = BeautifulSoup(html)
+
+#Retrieve all of the anchor tags
+tags = soup('a')
+for tag in tags:
+	print tag.get('href', None)
+
+
+# http://www.dr-chuck.com/page1.htm
